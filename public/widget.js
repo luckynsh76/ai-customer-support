a0464521dbe1bcc0ae6ed70f21f3ccff6e6fb2af
nshilucky76-cyber <nshilucky76@gmail.com>
2026-04-27 22:27:52 +0200

    fix chat toggle  + mobile popup

--- public/widget.js
+++ public/widget.js
@@ -69,15 +69,11 @@ style.innerHTML = `
   border-radius: 16px;
   box-shadow: 0 10px 30px rgba(0,0,0,0.2);
 
-  display: none;
+  display: flex;
   flex-direction: column;
   overflow: hidden;
 
   z-index: 9999;
-}
-
-.chat-widget.open {
-  display: flex;
 }
 
 .chat-toggle {
